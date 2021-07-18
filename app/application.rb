f70ab1a478cda56a77bb720ad96601c2223c3c01
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-07-18 15:17:08 -0400

    Completed assignment

--- app/application.rb
+++ app/application.rb
@@ -18,15 +18,23 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term) 
 
-    elsif req.path.match(/cart/)  #added by me
-      if @@cart.length > 0  #added by me
-          @@cart.each do |cart_item| #added by me
-            resp.write "#{cart_item}\n" #added by me
+
+    #Create a new route called /cart to show the items in your cart.
+    # --responds with empty cart message if the cart is empty
+    # --responds with a cart list if there is something in there
+    elsif req.path.match(/cart/)  
+      if @@cart.length > 0 
+          @@cart.each do |cart_item| 
+            resp.write "#{cart_item}\n"
           end
       else
         resp.write "Your cart is empty"
       end
 
+
+    # Create a new route called /add that takes in a GET param with the key item. This should check to see if that item is in @@items and add it to the cart if it is. Otherwise it should give an error
+    # --Will add an item that is in the @@items list
+    # --Will not add an item that is not in the @@items list
     elsif req.path.match(/add/)
       add_item = req.params["item"]
       # binding.pry
@@ -36,9 +44,6 @@ class Application
       else 
         resp.write "We don't have that item"
       end
-
-
-
 
     else
       resp.write "Path Not Found"
